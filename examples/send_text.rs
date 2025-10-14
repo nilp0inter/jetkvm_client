f239c1d51c630ffa593a2f314c1e10ab8e5fb41e
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2025-10-14 23:55:37 +0200

    refactor: reuse notepad example as send_text example

--- examples/send_text.rs
+++ examples/send_text.rs
@@ -1,9 +1,41 @@
 use anyhow::Result as AnyResult;
+use clap::Parser;
 use jetkvm_client::jetkvm_rpc_client::JetKvmRpcClient;
 use jetkvm_client::keyboard::*;
 use tokio::time::{sleep, Duration};
 use tracing::{error, info};
 use tracing_subscriber;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct CliConfig {
+    /// The host address to connect to.
+    #[arg(short = 'H', long)]
+    host: String,
+
+    /// The port number to use.
+    #[arg(short = 'p', long, default_value = "80")]
+    port: String,
+
+    /// The API endpoint.
+    #[arg(short = 'a', long, default_value = "/webrtc/session")]
+    api: String,
+
+    /// The password for authentication.
+    #[arg(short = 'P', long)]
+    password: String,
+
+    /// The text to send.
+    #[arg(short = 't', long)]
+    text: String,
+
+    /// Enable verbose logging (include logs from webrtc_sctp).
+    #[arg(short = 'v', long)]
+    verbose: bool,
+
+    #[arg(short = 'C', long, default_value = "cert.pem")]
+    ca_cert_path: String,
+}
 
 #[tokio::main]
 async fn main() -> AnyResult<()> {
@@ -15,10 +47,12 @@ async fn main() -> AnyResult<()> {
         .with_max_level(tracing::Level::DEBUG) // Set debug level
         .init();
 
+    let cli_config = CliConfig::parse();
+
     let mut client = JetKvmRpcClient::new(
-        "localhost:8000".to_string(),
-        "password".to_string(),
-        "/webrtc/session".to_string(),
+        cli_config.host,
+        cli_config.password,
+        cli_config.api,
         false,
     );
 
@@ -33,8 +67,8 @@ async fn main() -> AnyResult<()> {
     sleep(Duration::from_millis(250)).await;
     send_return(&client).await.ok();
     sleep(Duration::from_millis(250)).await;
-    info!("sending helloworld");
-    rpc_sendtext(&client, "Hello World").await.ok();
+    info!("sending text");
+    rpc_sendtext(&client, &cli_config.text).await.ok();
     sleep(Duration::from_millis(100)).await;
     send_ctrl_a(&client).await.ok();
     sleep(Duration::from_millis(100)).await;
