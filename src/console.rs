162bb862ec674c2187e026ee3d2147087f93152d
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2025-10-18 22:27:27 +0200

    fix: console map to stderr and fix control codes

--- src/console.rs
+++ src/console.rs
@@ -13,54 +13,59 @@ pub async fn open_console(client: &JetKvmRpcClient) -> AnyResult<Value> {
         .ok_or_else(|| anyhow::anyhow!("Serial client not connected"))?;
 
     serial_client.on_message(Box::new(move |msg| {
-        let mut stdout = io::stdout();
-        stdout.write_all(&msg.data).unwrap();
-        stdout.flush().unwrap();
+        let mut stderr = io::stderr();
+        stderr.write_all(&msg.data).unwrap();
+        stderr.flush().unwrap();
         Box::pin(async {})
     }));
 
     let mut stdin = io::stdin().keys();
-    let mut stdout = io::stdout().into_raw_mode()?;
+    let mut stderr = io::stderr().into_raw_mode()?;
 
-    loop {
-        if let Some(Ok(key)) = stdin.next() {
-            match key {
-                termion::event::Key::Ctrl('\\') => {
-                    break;
-                }
-                termion::event::Key::Char(c) => {
-                    client.send_serial(&[c as u8]).await?;
-                }
-                termion::event::Key::Ctrl(c) => {
-                    if c.is_alphabetic() {
-                        client.send_serial(&[(c as u8) - 96]).await?;
+    let result = async {
+        loop {
+            if let Some(Ok(key)) = stdin.next() {
+                match key {
+                    // termion::event::Key::Ctrl('\\') => {
+                    //     break;
+                    // }
+                    termion::event::Key::Char(c) => {
+                        client.send_serial(&[c as u8]).await?;
                     }
+                    termion::event::Key::Ctrl(c) => {
+                        if c.is_alphabetic() {
+                            client.send_serial(&[(c as u8) - 96]).await?;
+                        } else if c == '4' {  // Corresponds to Ctrl+\
+                            break;
+                        }
+                    }
+                    termion::event::Key::Backspace => {
+                        client.send_serial(&[8]).await?;
+                    }
+                    termion::event::Key::Esc => {
+                        client.send_serial(&[27]).await?;
+                    }
+                    termion::event::Key::Up => {
+                        client.send_serial(b"\x1b[A").await?;
+                    }
+                    termion::event::Key::Down => {
+                        client.send_serial(b"\x1b[B").await?;
+                    }
+                    termion::event::Key::Right => {
+                        client.send_serial(b"\x1b[C").await?;
+                    }
+                    termion::event::Key::Left => {
+                        client.send_serial(b"\x1b[D").await?;
+                    }
+                    _ => {}
                 }
-                termion::event::Key::Backspace => {
-                    client.send_serial(&[8]).await?;
-                }
-                termion::event::Key::Esc => {
-                    client.send_serial(&[27]).await?;
-                }
-                termion::event::Key::Up => {
-                    client.send_serial(b"\x1b[A").await?;
-                }
-                termion::event::Key::Down => {
-                    client.send_serial(b"\x1b[B").await?;
-                }
-                termion::event::Key::Right => {
-                    client.send_serial(b"\x1b[C").await?;
-                }
-                termion::event::Key::Left => {
-                    client.send_serial(b"\x1b[D").await?;
-                }
-                _ => {}
             }
         }
-    }
+        AnyResult::Ok(())
+    }.await;
 
-    stdout.flush()?;
-    drop(stdout);
+    stderr.flush()?;
+    drop(stderr);
 
-    Ok(json!({ "status": "ok" }))
+    result.map(|_| json!({ "status": "ok" }))
 }
